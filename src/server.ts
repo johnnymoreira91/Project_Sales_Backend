b3cb69470f9b91e829ca3601a019910c13bb1b5e
Johnny Moreira <jmoreira@engebras.com.br>
2022-02-10 16:03:46 -0300

    add morgan

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
 import '@controllers/UsersController'
+import morganMiddleware from './middlewares/morganMiddleware'
 
 const app = express()
+
+app.use(morganMiddleware)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// app.use(cors());
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
