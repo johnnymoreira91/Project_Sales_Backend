61541cf433ca264cb6dbc6158b946c48064192dd
Johnny Moreira <jmoreira@engebras.com.br>
2022-02-10 14:10:03 -0300

    fix all

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import '@controllers/UsersController'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+})
+
+export default app
