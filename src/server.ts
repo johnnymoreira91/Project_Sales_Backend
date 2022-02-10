9c1305c24cddd0aaef87e3eba2d9cbca5184ef60
Johnny Moreira <jmoreira@engebras.com.br>
2022-02-10 16:04:24 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import '@controllers/UsersController'
 import morganMiddleware from './middlewares/morganMiddleware'
+import cors from 'cors'
 
 const app = express()
 
 app.use(morganMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(cors());
+app.use(cors())
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
