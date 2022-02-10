0e6a0588db837fd96b96660406bc40688be2bc1d
Johnny Moreira <jmoreira@engebras.com.br>
2022-02-10 19:33:47 -0300

    add function connect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import app from './server'
 
-app.listen(3001, () => {
-  console.log('server running')
-})
+if (process.env.NODE_ENV === 'dev') {
+  Connect(3001, 'dev')
+} else if (process.env.NODE_ENV === 'prod') {
+  Connect(8080, 'prod')
+} else {
+  Connect(3001, 'NO_AMBIENT_DEFINED')
+}
+
+function Connect (port, stage) {
+  app.listen(port, () => {
+    console.log(`Running on ${stage} process`)
+  })
+}
